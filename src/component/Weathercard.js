4bfbd937a72a46578b35ab0a94161c2de3e07636
Bobby <bobbydura10@gmail.com>
2021-08-11 08:31:22 +0545

    WeatherIcon change according to Api weatherData

--- src/component/Weathercard.js
+++ src/component/Weathercard.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 
 const Weathercard = ({weatherInfo}) => {
 
-
+    const [ weatherState, setWeatherState] = useState("");
     const {
         temp,humidity,pressure,weathermood,name,speed,country,sunset,
     } = weatherInfo;
+
+    // To change the weather icon as per the real condition
+
+    useEffect(() => {
+        if(weathermood)
+        {
+            switch(weathermood)
+            {
+                case "Clouds":
+                    setWeatherState("wi-day-cloudy");
+                    break;
+
+                case "Haze":
+                    setWeatherState("wi-fog");
+                    break;
+
+                case "Clear":
+                    setWeatherState("wi-day-sunny");
+                    break;
+
+                case "Mist":
+                        setWeatherState("wi-dust");
+                        break;
+
+                default: 
+                setWeatherState("wi-day-sunny");
+                break;               
+            }
+        }
+
+       
+    }, [weathermood])
 
     // Converting the seconds into time
 
@@ -20,14 +52,14 @@ const Weathercard = ({weatherInfo}) => {
 
            <article className="widget">
                 <div className="weatherIcon">
-                    <i className={"wi wi-day-cloudy"}> </i>
+                    <i className={`wi ${weatherState}`}> </i>
                 </div>
 
                 <div className="weatherInfo">
                     <div className="temperature"> <span>{temp}&deg;</span> </div>
 
                     <div className="description">
-                        <div className="weatherCondition">  sunny </div>
+                        <div className="weatherCondition"> {weathermood}</div>
                         <div className="place"> {name}, {country}</div>
                     </div>
                 </div>
